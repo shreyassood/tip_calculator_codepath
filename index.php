65f8127962eb27c2c29324896d8fd0ea1e3f1f50
Shreyas Sood <shreyassood@gmail.com>
2017-01-12 01:14:05 +0530

    Added initial index page with an empty HTML template

---
+++ index.php
@@ -0,0 +1,14 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>The HTML5 Herald</title>
+  <meta name="description" content="A simple tip-calculator app">
+</head>
+
+<body>
+	
+</body>
+
+</html>
